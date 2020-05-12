74890f23626848a50b4f70b683c3790fbc875afa
Angel Kozlev <angelkozlev@gmail.com>
2020-05-12 16:07:47 +0100

    Updates

---
+++ precache-manifest.f12385f6cd558f5b705ca3bbf9ffa04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15efbccda2d5da93ed8773c6401c84ce",
+    "url": "/simple-react-todo/index.html"
+  },
+  {
+    "revision": "18caf013efbf70e4a123",
+    "url": "/simple-react-todo/static/css/main.fc0e32e3.chunk.css"
+  },
+  {
+    "revision": "b66da4b12dc5c8563cf5",
+    "url": "/simple-react-todo/static/js/2.29049983.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-todo/static/js/2.29049983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18caf013efbf70e4a123",
+    "url": "/simple-react-todo/static/js/main.3bdac204.chunk.js"
+  },
+  {
+    "revision": "71f6d11b79872f0ed785",
+    "url": "/simple-react-todo/static/js/runtime-main.9e9cf707.js"
+  }
+]);
